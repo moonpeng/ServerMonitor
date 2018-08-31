080d7d0a9999751582ca07a4e3c7ccb83c4f0425
wuxiaopeng <wupeng1056@vip.qq.com>
2018-08-31 10:44:07 +0800

    项目结构调整

--- src/Moon.AppMetricsMonitor/Startup.cs
+++ src/Moon.AppMetricsMonitor/Startup.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Moon.Formatters.AliTSDB;
 
 namespace Moon.AppMetricsMonitor
 {
